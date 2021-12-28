25d8c5de147f98a1dac4ef6a2447d423e3403d58
kaka567 <coderwh@163.com>
2021-12-28 23:08:09 +0800

    feature：适配M1模拟器

--- flutter_boost_3.0_demo/flutter_boost_dart/lib/pages/page_a.dart
+++ flutter_boost_3.0_demo/flutter_boost_dart/lib/pages/page_a.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_boost/boost_navigator.dart';
 import 'package:flutter_boost/flutter_boost.dart';
 
 class PageA extends StatelessWidget {
